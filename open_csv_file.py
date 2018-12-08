d2a30860ae2bf927dfa309eb908a89c3f20e356f
tbi2013 <45578127+tbi2013@users.noreply.github.com>
2018-12-07 22:11:44 -0500

    Update open_csv_file.py

--- open_csv_file.py
+++ open_csv_file.py
@@ -1,8 +1,8 @@
->> import csv
->> r = csv.reader(open('C:/Users/tbi2013/Downloads/Grade.csv', 'rb'))
->>> for row in r:
-...     print row
-...
+import csv
+r = csv.reader(open('C:/Users/tbi2013/Downloads/Grade.csv', 'rb'))
+for row in r:
+print row
+
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 _csv.Error: field larger than field limit (131072)
